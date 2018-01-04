e12b2b4c43fc0af2aeaedb2527bb45d4a640c311
Evan Carlinx <evan@carlin.com>
2018-01-04 08:45:06 -0800

    Sign in sign up buttons

--- app/src/main/java/com/banter/banter/MainActivity.java
+++ app/src/main/java/com/banter/banter/MainActivity.java
@@ -1,7 +1,9 @@
 package com.banter.banter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void SignInButtonPressed(View view) {
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
+    }
+
+    public void SignUpButtonPressed(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
