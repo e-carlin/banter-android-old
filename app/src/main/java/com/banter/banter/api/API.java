32096e3eece68321b4e52046aee128eb8dee62f8
Evan Carlin <evan.carlin@slalom.com>
2018-02-06 17:38:35 -0800

    Parameterize backend url

--- app/src/main/java/com/banter/banter/api/API.java
+++ app/src/main/java/com/banter/banter/api/API.java
@@ -9,10 +9,13 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.banter.banter.BuildConfig;
 import com.banter.banter.PlaidAddAccountActivity;
 import com.banter.banter.UserDetailsActivity;
 import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by evan.carlin on 2/6/2018.
@@ -21,18 +24,19 @@ import org.json.JSONObject;
 public class API {
     private static final String TAG = "APi";
 
-    private static final String API_BASE_URL = "localhost:???";
+    private static final String API_BASE_URL = BuildConfig.API_BASE_URL;
+    private static final String EXCHANGE_PUBLIC_TOKEN_ENDPOINT = "/exchange_plaid_public_token";
 
-    public static void sendPlaidPublicToken(Context ctx, JSONObject json) {
+    public static void sendPlaidPublicToken(Context ctx, JSONObject data) {
+        String url = API_BASE_URL+EXCHANGE_PUBLIC_TOKEN_ENDPOINT;
+        Log.e(TAG, "URL: "+url);
         Log.d(TAG, "Sending public token to our api");
         RequestQueue requestQueue = RequestQueueSingleton.getInstance(ctx).getRequestQueue();
 
-        String url = "https://api.ipify.org/?format=json";
-
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, API_BASE_URL+EXCHANGE_PUBLIC_TOKEN_ENDPOINT, data,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(String response) {
+                    public void onResponse(JSONObject response) {
                         // Display the response string.
                         System.out.println("Response is: " + response.toString());
                         ctx.startActivity(new Intent(ctx, UserDetailsActivity.class));
@@ -42,13 +46,20 @@ public class API {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.e(TAG, "Error sending plaid public token to our api: "+error);
+                try {
+                    Log.e(TAG, "Error message: " + new String(error.networkResponse.data, "UTF-8"));
+                }
+                catch (UnsupportedEncodingException e) {
+                    Log.e(TAG, "Error parsing the exchange_public_token error response body");
+                    Log.e(TAG, Log.getStackTraceString(e));
+                }
                 Log.e(TAG, Log.getStackTraceString(error));
 
                AlertDialog sendPublicTokenErrorAlertDialog = getSendPublicTokenErrorAlerDialog(ctx);
                sendPublicTokenErrorAlertDialog.show();
             }
         });
-        requestQueue.add(stringRequest);
+        requestQueue.add(jsonRequest);
     }
 
     private static AlertDialog getSendPublicTokenErrorAlerDialog(Context ctx) {
@@ -70,7 +81,6 @@ public class API {
                         ctx.startActivity(new Intent(ctx, UserDetailsActivity.class));
                     }
                 });
-
         return alertDialog;
     }
 
